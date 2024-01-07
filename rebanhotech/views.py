50f4c9dab1e695b9e954d1489359b1d081a7a56b
gwpoliveira <gwpoliveira@gmail.com>
2024-01-07 16:01:16 -0300

    Atualizações

--- rebanhotech/views.py
+++ rebanhotech/views.py
@@ -1,5 +1,8 @@
+from django.http import Http404
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
+from cadastro_animais.models import Animal
+from genealogia.models import Genealogia
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
@@ -7,6 +10,19 @@ from django.urls import reverse_lazy
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        try:
+            context['cadastro_animais'] = Animal.objects.all()[:10]
+            context['genealogia'] = Genealogia.objects.all()[:10]
+        except Animal.DoesNotExist:
+            context['cadastro_animais'] = None
+        except Genealogia.DoesNotExist:
+            context['genealogia'] = None
+
+        return context
     
 class UserProfileView(LoginRequiredMixin, UpdateView):
     model = User
